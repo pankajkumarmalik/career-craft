b9fbcf1fb56236eeef33c40a843e00135cffad24
Pankaj Kumar Malik <pankajkumarmalik6@gmail.com>
2025-04-24 15:52:54 +0530

    Create Feedback

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { vapi } from "@/lib/vapi.sdk";
 import { interviewer } from "@/constants";
+import { createFeedback } from "@/lib/actions/general.action";
 
 enum CallStatus {
   INACTIVE = "INACTIVE",
@@ -65,23 +66,24 @@ const Agent = ({
     };
   }, []);
 
-  const handleGenerateFeedback = async (messages: SavedMessage[]) => {
-    console.log("Generate feedback here.");
+  useEffect(() => {
+    const handleGenerateFeedback = async (messages: SavedMessage[]) => {
+      console.log("Generate feedback here.");
 
-    const { success, id } = {
-      success: true,
-      id: "feedback-id",
+      const { success, feedbackId: id } = await createFeedback({
+        interviewId: interviewId!,
+        userId: userId!,
+        transcript: messages,
+      });
+
+      if (success && id) {
+        router.push(`/interview/${interviewId}/feedback`);
+      } else {
+        console.log("Error saving feedback");
+        router.push("/");
+      }
     };
 
-    if (success && id) {
-      router.push(`/interview/${interviewId}/feedback`);
-    } else {
-      console.log("Error saving feedback");
-      router.push("/");
-    }
-  };
-
-  useEffect(() => {
     if (callStatus === CallStatus.FINISHED) {
       if (type === "generate") {
         router.push("/");
@@ -89,7 +91,7 @@ const Agent = ({
         handleGenerateFeedback(messages);
       }
     }
-  }, [messages, callStatus, type, userId]);
+  }, [messages, callStatus, type, userId, interviewId, router]);
 
   const handleCall = async () => {
     setCallStatus(CallStatus.CONNECTING);
